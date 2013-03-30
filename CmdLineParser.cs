b3ebcc76345ccfce270c0c1d1aa1298760207be3
Spencer Parkin (Avalanche) <Spencer.Parkin@disney.com>
2013-03-30 11:22:00 -0600

    more robust cmd-line parsing

---
+++ CmdLineParser.cs
@@ -0,0 +1,104 @@
+﻿// CmdLineParser.cs
+
+using System;
+using System.Collections.Generic;
+
+namespace CmdLineParser
+{
+    class CmdLineArgument
+    {
+        private static List< CmdLineArgument > cmdLineArgList;
+
+        public delegate void UnknownOptionFunc( string optionName );
+        public delegate void HandleOptionFunc( CmdLineArgument cmdLineArg );
+
+        public static UnknownOptionFunc unknownOptionFunc;
+        public HandleOptionFunc handleOptionFunc;
+
+        static CmdLineArgument()
+        {
+            unknownOptionFunc = null;
+        }
+
+        public CmdLineArgument( string optionName, int argCount = 0 )
+        {
+            this.optionName = optionName;
+            this.argCount = argCount;
+
+            if( cmdLineArgList == null )
+                cmdLineArgList = new List< CmdLineArgument >();
+            cmdLineArgList.Add( this );
+
+            handleOptionFunc = null;
+            optionGiven = false;
+
+            argOptionList = new List< string >();
+        }
+
+        public bool OptionGiven()
+        {
+            return optionGiven;
+        }
+
+        public string GetOptionArgument( int index )
+        {
+            if( index < 0 || index > argOptionList.Count - 1 )
+                return "";
+            return argOptionList[ index ];
+        }
+
+        private static CmdLineArgument LookupArg( string token )
+        {
+            foreach( CmdLineArgument cmdLineArg in cmdLineArgList )
+                if( cmdLineArg.optionName == token )
+                    return cmdLineArg;
+            return null;
+        }
+
+        public static bool ParseCmdLine( string[] cmdLine )
+        {
+            for( int index = 0; index < cmdLine.Length; index++ )
+            {
+                string token = cmdLine[ index ];
+                CmdLineArgument cmdLineArg = LookupArg( token );
+                if( cmdLineArg == null )
+                {
+                    if( unknownOptionFunc != null )
+                        unknownOptionFunc( token );
+                    return false;
+                }
+                else
+                {
+                    cmdLineArg.optionGiven = true;
+                    for( int argIndex = 0; argIndex < cmdLineArg.argCount; argIndex++ )
+                    {
+                        if( index + 1 + argIndex >= cmdLine.Length )
+                            return false;
+                        string optionArgString = cmdLine[ index + 1 + argIndex ];
+                        cmdLineArg.argOptionList.Add( optionArgString );
+                    }
+                    index += cmdLineArg.argCount;
+                }
+            }
+
+            // Now that everything is parsed, go call all registered handlers.
+            foreach( CmdLineArgument cmdLineArg in cmdLineArgList )
+                if( cmdLineArg.optionGiven && cmdLineArg.handleOptionFunc != null )
+                    cmdLineArg.handleOptionFunc( cmdLineArg );
+
+            return true;
+        }
+
+        private string optionName;
+        private int argCount;
+        private bool optionGiven;
+        private List< string > argOptionList;
+
+        public string OptionName
+        {
+            get { return optionName; }
+        }
+    }
+}
+
+// CmdLineParser.cs
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using CmdLineParser;
 
 namespace CSharpMaze
 {
@@ -13,32 +14,48 @@ namespace CSharpMaze
         // This is the program entry point.
         static void Main( string[] args )
         {
+            CmdLineArgument rectArg = new CmdLineArgument( "rect", 2 );
+            CmdLineArgument circArg = new CmdLineArgument( "circ", 1 );
+            CmdLineArgument imageFileArg = new CmdLineArgument( "imageFile", 1 );
+
+            if( !CmdLineArgument.ParseCmdLine( args ) )
+            {
+                System.Console.WriteLine( "Usage: CSharpMaze [rect|circle] [(rows,cols)|rings] [imageFile <file>]" );
+                return;
+            }
+
+            if( rectArg.OptionGiven() && circArg.OptionGiven() )
+            {
+                System.Console.WriteLine( "Supply rectangular maze arguments or circular maze arguments, but not both." );
+                return;
+            }
+
             ShapeGraph graph = null;
 
-            if( args.Length > 0 )
+            if( rectArg.OptionGiven() )
             {
-                if( args[0] == "rect" )
-                {
-                    if( args.Length > 2 )
-                    {
-                        int rows = Convert.ToInt32( args[1] );
-                        int cols = Convert.ToInt32( args[2] );
-                        graph = new RectangularGraph( rows, cols );
-                    }
-                }
-                else if( args[0] == "circle" )
-                {
-                    if( args.Length > 1 )
-                    {
-                        int concentricCircles = Convert.ToInt32( args[1] );
-                        graph = new CircularGraph( concentricCircles );
-                    }
-                }
+                int rows = Convert.ToInt32( rectArg.GetOptionArgument(0) );
+                int cols = Convert.ToInt32( rectArg.GetOptionArgument(1) );
+                graph = new RectangularGraph( rows, cols );
             }
-            
+            else if( circArg.OptionGiven() )
+            {
+                int rings = Convert.ToInt32( circArg.GetOptionArgument(0) );
+                graph = new CircularGraph( rings );
+            }
+
             if( graph == null )
             {
-                System.Console.WriteLine( "Usage: CSharpMaze [rect|circle] [(rows,cols)|circle]" );
+                System.Console.WriteLine( "Please supply the maze type: \"rect\" or \"circ\"." );
+                return;
+            }
+
+            String imageFile = null;
+            if( imageFileArg.OptionGiven() )
+                imageFile = imageFileArg.GetOptionArgument( 0 );
+            else
+            {
+                System.Console.WriteLine( "Please supply the image file name." );
                 return;
             }
 
@@ -51,7 +68,7 @@ namespace CSharpMaze
 
             Bitmap bitmap = new Bitmap( 1024, 1024, System.Drawing.Imaging.PixelFormat.Format24bppRgb );
             maze.Render( bitmap );
-            bitmap.Save( "Maze.jpg" );
+            bitmap.Save( imageFile );
         }
     }
 }
